9979c0aa9aad3c0ffc5317d9c192eed7abcb7969
Moyseys <moyseys.veroni@gmail.com>
2024-10-14 15:59:41 -0300

    algoritmo quadro nomenclaturas

--- 2 semestre/python/quadro_algoritmo/Main2.java
+++ 2 semestre/python/quadro_algoritmo/Main2.java
@@ -1,5 +1,3 @@
 public class Main2 {
-    public static void main(String[] args) {
-       
-    }
+    
 }
--- 2 semestre/python/quadro_algoritmo/regrescao_linar.py
+++ 2 semestre/python/quadro_algoritmo/regrescao_linar.py
@@ -25,36 +25,32 @@ df = pd.DataFrame({
     'mes': x.flatten(),
     'gastos': y
 })
-print(x, y)
 modelo = LinearRegression()
 
 # Treinando o modelo
 # x Variável independente
 # y Variavel dependente
 modelo.fit(x, y)
-y_pred = modelo.predict(x)
 
 # Identificar o próximo mês com base no último mês disponível
 ultimo_mes = max(despesas_formatada.keys())
-proximo_mes = ultimo_mes + 1 if ultimo_mes < 12 else 1  # Se o último mês for dezembro, volta para janeiro (1)
+proximo_mes = ultimo_mes + 1 if ultimo_mes < 12 else 1
 
 # Prever o gasto para o próximo mês
 gasto_proximo_mes = modelo.predict(np.array([[proximo_mes]]))
-print(gasto_proximo_mes)
 df = df._append({'mes': proximo_mes, 'gastos': gasto_proximo_mes[0]}, ignore_index=True)
 # Atualizar a lista de meses
 meses_nome = []
 for m in df['mes']:
-    print(m)
     meses_nome.append(calendar.month_name[int(m)])
 
 # Recalcular as previsões para todos os meses incluindo o próximo   
 x_novo = np.array(df['mes']).reshape(-1, 1)
-y_pred_novo = modelo.predict(x_novo)
+y_pred = modelo.predict(x_novo)
 
 plt.figure(figsize=(10, 6))
 plt.plot(meses_nome, df['gastos'], marker='o', label='Gastos Mensais')
-plt.plot(meses_nome, y_pred_novo, color='red', linestyle='--', label='Previsão de Gastos')
+plt.plot(meses_nome, y_pred, color='red', linestyle='--', label='Previsão de Gastos')
 plt.title('Gastos Mensais e Previsão')
 plt.xlabel('Meses')
 plt.ylabel('Gastos')
